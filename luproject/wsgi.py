634f58e9aa72ec062b8eae521269b363fef015a3
Mike Cardona <mikecardona076@gmail.com>
2020-09-17 15:33:25 -0700

    #First Commit

---
+++ luproject/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for luproject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'luproject.settings')
+
+application = get_wsgi_application()
