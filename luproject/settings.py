c3a3497c867ac082ce7875fcc4651ff704c60579
Mike Cardona <mikecardona076@gmail.com>
2020-09-17 17:37:19 -0700

    #0.0

--- luproject/settings.py
+++ luproject/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qie-)*x39+z@4=w(s85qyzykhg#j*tdjk6ihkbh@u)*rmsk&65'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
